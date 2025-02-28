7fe7e62211bafa4b7884a3e8192118ab84511193
JadahH <Jhol1918@students.vsu.edu>
2025-02-27 22:56:38 -0500

    Added documentation

--- Num/Range.cs
+++ Num/Range.cs
@@ -3,11 +3,32 @@ using System.Collections.Generic;
 
 namespace Num;
 
+/// <summary>
+    /// Represents an exception thrown when an invalid sequence is generated.
+    /// Specifically, this exception is thrown when three consecutive numbers are less than or equal to 0.5.
+    /// </summary>
 
 public class BadSequenceException : Exception
     {
+       /// <summary>
+        /// Initializes a new instance of the <see cref="BadSequenceException"/> class.
+        /// </summary>  
         public BadSequenceException() { }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="BadSequenceException"/> class with a specified error message.
+        /// </summary>
+        /// <param name="message">The message that describes the error.</param>
+        /// 
+
         public BadSequenceException(string message) : base(message) { }
+
+         /// <summary>
+        /// Initializes a new instance of the <see cref="BadSequenceException"/> class with a specified error message
+        /// and a reference to the inner exception that is the cause of this exception.
+        /// </summary>
+        /// <param name="message">The error message that explains the reason for the exception.</param>
+        /// <param name="innerException">The exception that is the cause of the current exception.</param>
         public BadSequenceException(string message, Exception innerException) : base(message, innerException) { }
 
     }
