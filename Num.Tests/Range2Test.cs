f6d9d03882d30fcc142061bfccf6977c66ece071
JadahH <Jhol1918@students.vsu.edu>
2025-03-04 10:58:05 -0500

    Added a  test that constructs a FloatNumberIterator from a sequence of doubles

---
+++ Num.Tests/Range2Test.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+using Xunit;
+using System.IO;
+
+namespace Num.Tests;
+
+public class Range2Tests
+{
+      [Fact]
+        public void GroupedQuarters()
+        {
+            var testSequence = new List<double> { 0.1, 0.3, 0.6, 0.9 };
+            var iterator = new FloatNumberIterator(testSequence);
+            var enumerator = iterator.GetEnumerator();
+            List<Range> quarters = new List<Range>();
+
+            while (enumerator.MoveNext())
+            {
+                quarters.Add(new Range(enumerator.Current));
+            }
+
+            var groupedQuarters = quarters.GroupBy(q => q.GetHashCode())
+                                      .OrderBy(g => g.Key);
+
+            Assert.Equal(4, groupedQuarters.Count());
+        }
+
+
+
+}
